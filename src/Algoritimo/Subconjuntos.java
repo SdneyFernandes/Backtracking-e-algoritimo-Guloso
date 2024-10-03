6665164e75bd2c6ddca7982975b62857d1a666cc
SdneyFernandes <fsidney987@gmail.com>
2024-10-03 00:25:08 -0300

    retificando conteudo classes

--- src/Algoritimo/Subconjuntos.java
+++ src/Algoritimo/Subconjuntos.java
@@ -1,10 +1,55 @@
 package Algoritimo;
 
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author fsdney
  */
 
+
 public class Subconjuntos {
 
+    
+    void encontrarSubconjuntos(int[] S, List<Integer> subconjuntoAtual, int indiceAtual, int n, List<List<Integer>> soluções) {
+        
+        if (subconjuntoAtual.size() == n) {
+            soluções.add(new ArrayList<>(subconjuntoAtual)); 
+            return; 
+        }
+
+       
+        for (int i = indiceAtual; i < S.length; i++) {
+           
+            subconjuntoAtual.add(S[i]);
+
+            
+            encontrarSubconjuntos(S, subconjuntoAtual, i + 1, n, soluções);
+
+            
+            subconjuntoAtual.remove(subconjuntoAtual.size() - 1);
+        }
+    }
+
+   
+    void calcularSubconjuntos(int[] S, int n) {
+        List<Integer> subconjuntoAtual = new ArrayList<>();
+        List<List<Integer>> soluções = new ArrayList<>();
+        
+        encontrarSubconjuntos(S, subconjuntoAtual, 0, n, soluções);
+        
+        
+        for (List<Integer> subconjunto : soluções) {
+            System.out.println(subconjunto);
+        }
+    }
+
+   
+    public static void main(String[] args) {
+        Subconjuntos obj = new Subconjuntos();
+        int[] S = {1, 2, 3}; 
+        int n = 2;          
+        obj.calcularSubconjuntos(S, n);
+    }
 }
+
--- src/Algoritimo/Troco.java
+++ src/Algoritimo/Troco.java
@@ -1,10 +1,60 @@
 package Algoritimo;
 
+import java.util.Arrays;
 
 /**
  * @author fsdney
  */
 
+
 public class Troco {
 
+    
+    void darTroco(int quantia, int[] moedas) {
+        
+        Arrays.sort(moedas);
+        reverseArray(moedas); 
+
+        
+        int contadorMoedas = 0;
+
+        
+        for (int i = 0; i < moedas.length; i++) {
+            
+            while (quantia >= moedas[i]) {
+                quantia -= moedas[i]; 
+                contadorMoedas++; 
+            }
+        }
+
+        
+        if (quantia > 0) {
+            System.out.println("Não é possível dar troco para a quantia " + quantia);
+        } else {
+           
+            System.out.println("Número total de moedas: " + contadorMoedas);
+        }
+    }
+
+    
+    void reverseArray(int[] array) {
+        int start = 0;
+        int end = array.length - 1;
+        while (start < end) {
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+   
+    public static void main(String[] args) {
+        Troco obj = new Troco();
+        int quantia = 18; 
+        int[] moedas = {5, 2, 1}; 
+        obj.darTroco(quantia, moedas);
+    }
 }
+
